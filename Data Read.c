6a16683994338b7ec683d734f27177d390f9a789
Ekaterina25 <32367612+Ekaterina25@users.noreply.github.com>
2018-05-31 20:40:36 +0300

    Добавила фун-ю Data Read

---
+++ Data Read.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <locale.h>
+void Archive(const char* FileNameIn, const char* FileNameOut);
+void DeArchive(const char* FileNameIn, const char* FileNameOut);
+void Compare_File(const char* FileNameIn, const char* FileNameOut);
+void DataRead(const char* FileNameIn);
+
+int main()
+{
+setlocale(LC_ALL, "Rus");
+	const char* ArchFileNameIn = "C:\\12452346\\1234.pdf";
+	const char* ArchFileNameOut = "C:\\12452346\\1234.re";
+	const char* DeArchFileNameIn = ArchFileNameOut;
+	const char* DeArchFileNameOut = "C:\\12452346\\1234.re1";
+
+	Archive(ArchFileNameIn, ArchFileNameOut);
+	DeArchive(DeArchFileNameIn, DeArchFileNameOut);
+	Compare_File(ArchFileNameIn, DeArchFileNameOut);
+       system("pause");
+	return 0;
+}
+void DataRead(const char* FileNameIn)
+{
+	printf("Чтение данных..\n");
+	input = fopen(FileNameIn, "rb");
+	printf("%s\n", FileNameIn);
+	if (input == NULL) return;
+	unsigned char byte_input = 0;
+	fscanf_s(input, "%c", &byte_input);
+void Archive(const char* FileNameIn, const char* FileNameOut)
+{
+	printf("Архивация..\n\n");
+	output = fopen(FileNameOut, "wb");
+	if (output == NULL) printf("error");
+	DataRead(FileNameIn);
+	if (length_registr == 0)
+	{
+		printf("Файл пуст или не существует!\n");
+		return;
+	}
+}
