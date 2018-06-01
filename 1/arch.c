872aa353f5e8452c11bb883ff049a2136fa0ba9f
Kate <katee2014@bk.ru>
2018-06-01 09:44:01 +0300

    Добавила функцию main

--- 1/arch.c
+++ 1/arch.c
@@ -2,9 +2,18 @@
 #include <locale.h>
 
 
-int main(void)
+int main()
 {
+	setlocale(LC_ALL, "Rus");
+	const char* ArchFileNameIn = "C:\\1234\\2.pdf";
+	const char* ArchFileNameOut = "C:\\1234\\1234.123";
+	const char* DeArchFileNameIn = ArchFileNameOut;
+	const char* DeArchFileNameOut = "C:\\1234\\1234.pdf";
 
- 
+	Archive(ArchFileNameIn, ArchFileNameOut);
+	DeArchive(DeArchFileNameIn, DeArchFileNameOut);
+	Compare_File(ArchFileNameIn, DeArchFileNameOut);
 
+	system("pause");
+	return 0;
 }
