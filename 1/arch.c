aee4c0c30600109b0306cb916b3e310c68b06fdd
sayonarabtch <sayonarabtch@gmail.com>
2018-06-04 22:26:45 +0300

    1 файл ввода, 1 файл ввода/вывода, 1 файл вывода архивированного файла структура для архиватора

--- 1/arch.c
+++ 1/arch.c
@@ -1,1 +1,17 @@
 #include <stdio.h>
+#include <locale.h>
+
+FILE *input;
+FILE *input2;
+FILE *output;
+
+///archive
+struct Registr
+{
+	char symbol;
+	int quantity;
+	float frequency;
+	unsigned long long code;//8 byte
+	unsigned char length_code;
+};
+int quant_reg = 0;
