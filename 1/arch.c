83ed5f310eb3105d0d7e592e502c26faec76af12
Ekaterina25 <32367612+Ekaterina25@users.noreply.github.com>
2018-06-06 19:20:24 +0300

    Чтение

--- 1/arch.c
+++ 1/arch.c
@@ -3,7 +3,15 @@
 
 
 void Archive(const char* FileNameIn, const char* FileNameOut);
+void DataRead(const char* FileNameIn);
+void Create_Registr(struct Registr *datasymbol);
+
+
+//dearchive
 void DeArchive(const char* FileNameIn, const char* FileNameOut);
+
+
+
 void Compare_File(const char* FileNameIn, const char* FileNameOut);
 
 int main(void)
@@ -20,4 +28,39 @@ int main(void)
 
 	system("pause");
 	return 0;
+}
+void Archive(const char* FileNameIn, const char* FileNameOut)
+{
+	printf("Архивация..\n\n");
+	output = fopen(FileNameOut, "wb");
+	if (output == NULL) printf("error");
+	DataRead(FileNameIn);
+	if (length_registr == 0)
+	{
+		printf("Файл пуст или не существует!\n");
+		return;
+	}
+struct Registr *DataSymbol;
+	DataSymbol = (int*)malloc(length_registr * sizeof(struct Registr));
+	Create_Registr(DataSymbol);
+}
+
+void DataRead(const char* FileNameIn)
+{
+	printf("Чтение данных..\n");
+	input = fopen(FileNameIn, "rb");
+	printf("%s\n", FileNameIn);
+	if (input == NULL) return;
+	unsigned char byte_input = 0;
+	fscanf_s(input, "%c", &byte_input);
+	while (!feof(input))
+	{
+
+		if (quantity_byte[byte_input] == 0) length_registr++;
+		quantity_byte[byte_input]++;
+		fscanf_s(input, "%c", &byte_input);
+	}
+	length_input_file = ftell(input);
+	fclose(input);
+	printf("OK\n\n");
 }
