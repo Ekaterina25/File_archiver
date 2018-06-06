26412e9348d29e153e78fbb473a061fd7b225147
sayonarabtch <sayonarabtch@gmail.com>
2018-06-06 16:51:43 +0300

    .

--- 1/arch.c
+++ 1/arch.c
@@ -5,6 +5,11 @@ FILE *input;
 FILE *input2;
 FILE *output;
 
+void Create_Registr(struct Registr *datasymbol);
+void DataSort(struct Registr *datasymbol);
+void CreateCode(struct Registr *datasymbol, int range_start, int range_stop);
+void CreateLibrary(struct Registr *datasymbol);
+void Encoding(struct Registr *datasymbol, const char* FileNameIn);
 ///archive
 struct Registr
 {
@@ -14,7 +19,7 @@ struct Registr
 	unsigned long long code;//8 byte
 	unsigned char length_code;
 };
-int quant_reg = 0;git
+int quant_reg = 0;
 
 
 ///dearchive
@@ -25,3 +30,26 @@ struct Registr_dea
 	unsigned long long code;//8 byte
 };
 char quant_reg_dea = 0;
+
+void Archive(const char* FileNameIn, const char* FileNameOut)
+{
+	printf("Архивация..\n\n");
+	fopen_s(output, FileNameOut, "wb");
+	
+	if (output == NULL) printf("error");
+	DataRead(FileNameIn);
+	if (length_registr == 0)
+	{
+		printf("Файл пуст или не существует!\n");
+		return;
+	}
+	struct Registr *DataSymbol;
+	DataSymbol = (int*)malloc(length_registr * sizeof(struct Registr));
+	Create_Registr(DataSymbol);
+	DataSort(DataSymbol);
+	CreateCode(DataSymbol, 0, quant_reg);
+	CreateLibrary(DataSymbol);
+	Encoding(DataSymbol, FileNameIn);
+	printf("Архивация завершена\nсжато в %f раз(а), процент сжатия: %f\n\n",
+		length_input_file / length_output_file, 100 * (1 - length_output_file / length_input_file));
+}
