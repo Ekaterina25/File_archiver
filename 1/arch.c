d7701c846ccae44ecc1307686b3b842fdc848752
Ekaterina25 <32367612+Ekaterina25@users.noreply.github.com>
2018-06-09 08:35:48 +0300

    Окончательный код

--- 1/arch.c
+++ 1/arch.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <locale.h>
+
 FILE *input;
 FILE *output;
 
@@ -13,12 +14,18 @@ struct Registr
 	unsigned char length_code;
 };
 int quant_reg = 0;
+
+
 void Archive(const char* FileNameIn, const char* FileNameOut);
 void DataRead(const char* FileNameIn);
 void Create_Registr(struct Registr *datasymbol);
 void DataSort(struct Registr *datasymbol);
 void CreateCode(struct Registr *datasymbol, int range_start, int range_stop);
 void WriteCode(struct Registr *datasymbol, char code, int range_start, int range_stop);
+void CreateLibrary(struct Registr *datasymbol);
+void WriteToFile(unsigned long long bits, char quantity_bit);
+void Encoding(struct Registr *datasymbol, const char* FileNameIn);
+void CloseOutPutFile();
 
 
 ///archive
@@ -29,37 +36,43 @@ char IndexBTF = 7;
 float length_input_file = 1;
 float length_output_file = 1;
 
-int main(void)
+
+int main()
 {
-    setlocale(LC_ALL, "Rus");
+	setlocale(LC_ALL, "Rus");
 	const char* ArchFileNameIn = "C:\\1234\\2.pdf";
-	const char* ArchFileNameOut = "C:\\1234\\1234.123";
-	Archive(ArchFileNameIn, ArchFileNameOut);
+	const char* ArchFileNameOut = "C:\\1234\\1.txt";
+    Archive(ArchFileNameIn, ArchFileNameOut);
 
 	system("pause");
 	return 0;
 }
+
 void Archive(const char* FileNameIn, const char* FileNameOut)
 {
-	printf("Архивация...\n\n");
+	printf("Архивация..\n\n");
 	output = fopen(FileNameOut, "wb");
 	if (output == NULL) printf("error");
 	DataRead(FileNameIn);
 	if (length_registr == 0)
 	{
-		printf("Файл пустой или не существует!\n");
+		printf("Файл пуст или не существует!\n");
 		return;
 	}
-struct Registr *DataSymbol;
+	struct Registr *DataSymbol;
 	DataSymbol = (int*)malloc(length_registr * sizeof(struct Registr));
 	Create_Registr(DataSymbol);
 	DataSort(DataSymbol);
 	CreateCode(DataSymbol, 0, quant_reg);
+	CreateLibrary(DataSymbol);
+	Encoding(DataSymbol, FileNameIn);
+	printf("Архивация завершена\nсжато в %f раз(а), процент сжатия: %f\n\n",
+		length_input_file / length_output_file, 100 * (1 - length_output_file / length_input_file));
 }
 
 void DataRead(const char* FileNameIn)
 {
-	printf("Чтение данных...\n");
+	printf("Чтение данных..\n");
 	input = fopen(FileNameIn, "rb");
 	printf("%s\n", FileNameIn);
 	if (input == NULL) return;
@@ -76,6 +89,7 @@ void DataRead(const char* FileNameIn)
 	fclose(input);
 	printf("OK\n\n");
 }
+
 void Create_Registr(struct Registr *datasymbol)
 {
 	printf("Создание регистра данных\n");
@@ -110,12 +124,14 @@ void DataSort(struct Registr *datasymbol)
 				dop = *(datasymbol + i);
 				*(datasymbol + i) = *(datasymbol + i + 1);
 				*(datasymbol + i + 1) = dop;
+
 			}
 		}
-	
+
 	}
 	printf("OK\n\n");
 }
+
 void CreateCode(struct Registr *datasymbol, int range_start, int range_stop)
 {
 	if (range_stop - range_start == quant_reg) printf("Создание кодов зашифровки..\n");
@@ -146,3 +162,66 @@ void WriteCode(struct Registr *datasymbol, char code, int range_start, int range
 		(datasymbol + i)->length_code++;
 	}
 }
+
+
+void CreateLibrary(struct Registr *datasymbol)
+{
+	printf("Создание библиотеки зашифровки..\n");
+	WriteToFile(quant_reg, 16);
+	unsigned long long quantity_bits = 0;
+	for (int i = 0; i < quant_reg; i++)
+		quantity_bits += (datasymbol + i)->length_code*(datasymbol + i)->quantity;
+	WriteToFile(quantity_bits, 64);
+
+	for (int i = 0; i < quant_reg; i++)
+	{
+		WriteToFile((datasymbol + i)->symbol, 8);
+		WriteToFile((datasymbol + i)->length_code, 8);
+		WriteToFile((datasymbol + i)->code, (datasymbol + i)->length_code);
+	}
+	printf("OK\n\n");
+}
+
+void WriteToFile(unsigned long long bits, char quantity_bit)
+{
+	for (int i = quantity_bit - 1; i >= 0; i--)
+	{
+		ByteToFile |= (((bits >> i) % 2) << IndexBTF);
+		IndexBTF--;
+		if (IndexBTF == -1)
+		{
+			fprintf(output, "%c", ByteToFile);//отправить байт в файл
+			ByteToFile = 0;
+			IndexBTF = 7;
+		}
+	}
+
+}
+
+void Encoding(struct Registr *datasymbol, const char* FileNameIn)
+{
+	printf("Архивирование..\n");
+
+	input = fopen(FileNameIn, "rb");
+	char byte_input = 0;
+	int pos_DS;
+	fscanf_s(input, "%c", &byte_input);
+	while (!feof(input))//чтение данных
+	{
+		pos_DS = 0;
+		while (byte_input != (datasymbol + pos_DS)->symbol) pos_DS++;//поиск нужной записи регистра
+		WriteToFile((datasymbol + pos_DS)->code, (datasymbol + pos_DS)->length_code);
+		fscanf_s(input, "%c", &byte_input);
+	}
+	fclose(input);
+	CloseOutPutFile();
+	printf("OK\n\n");
+}
+
+void CloseOutPutFile()
+{
+	if (IndexBTF != 7) WriteToFile(0, IndexBTF + 1);
+	length_output_file = ftell(output);
+	fclose(output);
+}
+
